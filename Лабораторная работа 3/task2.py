abb20ca04f3ba24a50a8d616229092c31d388083
Ksenia2705 <kchepkalenko@gmail.com>
2024-11-02 04:23:40 +0300

    task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,71 +1,22 @@
-# TODO  Напишите функцию count_letters
+# TODO Напишите функцию find_common_participants
 
-def count_letters(text):
-    lower_text = text.lower()
-    count_dict = {}
-    for i in lower_text:
-         if i.isalpha():
-             if i in count_dict:
-                 count_dict[i] += 1
-             else:
-                 count_dict[i] = 1
+def find_common_participants(str1, str2, razdelitl=","):
+    new_str1 = str1.split(razdelitl)
+    new_str2 = str2.split(razdelitl)
 
-    return count_dict
+    set1 = set(new_str1)
+    set2 = set(new_str2)
 
+    intersection_set = set1.intersection(set2)
 
+    new_list = list(intersection_set)
 
-# TODO Напишите функцию calculate_frequency
-def calculate_frequency(kolvo_dict):
-    frequency_dict = {}
-    total_count = sum(kolvo_dict.values())
-
-    for letter, count in kolvo_dict.items():
-        frequency_dict[letter] = round(count/total_count, 2)
-
-    return frequency_dict
+    return new_list
 
 
-main_str = """
-У лукоморья дуб зелёный;
-Златая цепь на дубе том:
-И днём и ночью кот учёный
-Всё ходит по цепи кругом;
-Идёт направо — песнь заводит,
-Налево — сказку говорит.
-Там чудеса: там леший бродит,
-Русалка на ветвях сидит;
-Там на неведомых дорожках
-Следы невиданных зверей;
-Избушка там на курьих ножках
-Стоит без окон, без дверей;
-Там лес и дол видений полны;
-Там о заре прихлынут волны
-На брег песчаный и пустой,
-И тридцать витязей прекрасных
-Чредой из вод выходят ясных,
-И с ними дядька их морской;
-Там королевич мимоходом
-Пленяет грозного царя;
-Там в облаках перед народом
-Через леса, через моря
-Колдун несёт богатыря;
-В темнице там царевна тужит,
-А бурый волк ей верно служит;
-Там ступа с Бабою Ягой
-Идёт, бредёт сама собой,
-Там царь Кащей над златом чахнет;
-Там русский дух… там Русью пахнет!
-И там я был, и мёд я пил;
-У моря видел дуб зелёный;
-Под ним сидел, и кот учёный
-Свои мне сказки говорил.
-"""
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
 
-
-result = calculate_frequency(count_letters(main_str))
-
-for letter, frequency in result.items():
-    print(f"{letter}: {frequency:.2f}")
-
-
-# TODO Распечатайте в столбик букву и её частоту в тексте
+result = find_common_participants(participants_first_group, participants_second_group, '|')
+print(result)
+# TODO Проверьте работу функции с разделителем отличным от запятой
